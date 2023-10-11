6f4310ea265ee71d505514952107e162be7d0937
yuuu022 <11056022@ntub.edu.tw>
2023-10-11 16:30:37 +0800

    urls.py, views.py, settings.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from mysite.models import Post #mysite這個資料夾中匯入先前已設好的東西
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+    post = Post.objects.get(slug=slug) 
+    #select * from post where slug=%slug
+    return render(request, 'post.html', locals())
+    
+
+
+'''
+def homepage(request):
+    posts = Post.objects.all() #select * from post
+    post_lists = list()
+    for counter,post in enumerate(posts):
+        post_lists.append(f'No. {counter} {post} <br>') #Ff都可 格式化字串 <br>換行
+    return HttpResponse(post_lists)
+'''
