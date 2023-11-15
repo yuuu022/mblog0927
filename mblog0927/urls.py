7b03a35506a7c1af1fb04f772498d6992424f433
yuuu022 <11056022@ntub.edu.tw>
2023-11-15 16:40:56 +0800

    1115(網址)

--- mblog0927/urls.py
+++ mblog0927/urls.py
@@ -21,5 +21,9 @@ from mysite import views as mv
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',mv.homepage, name="homepage"),
-    path('post/<slug:slug>/', mv.showpost, name="showpost")
+    path('post/<slug:slug>/', mv.showpost, name="showpost"),
+    path('about/',mv.about),
+    path('about/<int:num>', mv.about,{'num':1}),
+    path('post/<int:yr>/<int:mon>/<int:day>/<int:post_num>/',
+         mv.Post, name='post-url')
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -19,7 +19,27 @@ def showpost(request, slug):
             return redirect("/")
     except:
         return redirect("/")
-
+import random
+def about(request, num=-1):
+    quotes=['今日事，今日畢',
+            '要怎麼收穫，先那麼栽',
+            '知識就是力量',
+            '一個人的個性就是他的命運']    
+    if num==-1 or num>4:
+        quote = random.choice(quotes)
+    else:
+        quote = quotes[num]
+    return render(request, 'about.html', locals())
+    
+    mhtml = f'''
+<html>
+<body>
+<h1>I</h1>
+<h3>am in NTUB</h3>
+<h2>{num}</h2>
+</body></html>
+'''
+    return HttpResponse(mhtml)
 '''
 def showpost(request, slug):
     post = Post.objects.get(slug=slug) 
